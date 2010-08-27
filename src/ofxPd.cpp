70a46483ce4c94c453d5698d54f97a452e9294b0
damian <damian@frey.NOSPAMco.nz>
2010-08-27 10:46:02 +0200

    brought in Zach's input stuff
    	modified:   src/ofxPd.cpp
    	modified:   src/ofxPd.h

--- src/ofxPd.cpp
+++ src/ofxPd.cpp
@@ -1,11 +1,43 @@
 /*
- *  ofxPd.cpp
- *  luciolinae_ctl
- *
- *  Created by damian on 12/08/10.
- *  Copyright 2010 frey damian@frey.co.nz. All rights reserved.
- *
+ 
+ ofxPd v0.02 
+ Copyright 2010 by Damian Stewart, Zach Liebermann.
+ 
+ this code uses code from pdlib "AudioOutput.h", so pdlib license is included here: 
+ 
+ AudioOutput.h
+ PdLib v0.3
+ 
+ Copyright 2010 Bryan Summersett. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are
+ permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this list of
+ conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ of conditions and the following disclaimer in the documentation and/or other materials
+ provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY BRYAN SUMMERSETT ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BRYAN SUMMERSETT OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ The views and conclusions contained in the software and documentation are those of the
+ authors and should not be interpreted as representing official policies, either expressed
+ or implied, of Bryan Summersett.
+ 
+ 
+ 
  */
+
 
 #include "ofxPd.h"
 #include <assert.h>
@@ -20,13 +52,15 @@ extern "C"
 extern void sched_audio_callbackfn(void);
 };
 
-void ofxPd::setup( string _lib_dir, int _out_chans, int _bitrate, int _block_size )
+void ofxPd::setup( string _lib_dir, int _in_chans, int _out_chans, int _bitrate, int _block_size )
 {
 	lib_dir = ofToDataPath(_lib_dir);
 	out_chans = _out_chans;
+	in_chans = _in_chans;
 	bitrate = _bitrate;
 	block_size = _block_size;
 }
+
 
 void ofxPd::addOpenFile( string file_path )
 {
@@ -80,7 +114,7 @@ void ofxPd::threadedFunction()
 	
 	int sound_rate = bitrate;
 	int n_out_channels = out_chans;
-	int n_in_channels = 0;
+	int n_in_channels = in_chans;
 	sys_main( lib_dir.c_str(), externs_cat.c_str(), open_files_cat.c_str(), search_path_cat.c_str(),
 			 sound_rate, block_size, n_out_channels, n_in_channels );
 
@@ -126,3 +160,56 @@ void ofxPd::renderAudio( float* output, int bufferSize, int nChannels )
 	
 }
 
+void ofxPd::renderAudio( float * input, float* output, int bufferSize, int nChannels ){
+	// adapted from AudioOutputController by bryan summerset
+	
+	// sys_schedblocksize is How many frames we have per PD dsp_tick
+    // inNumberFrames is how many frames have been requested
+	int inNumberFrames = bufferSize;
+    
+    // Make sure we're dealing with evenly divisible numbers between
+    // the number of frames requested vs the block size for a given PD dsp tick.
+    //Otherwise this looping scheme we're doing below doesn't make much sense.
+    assert(inNumberFrames % sys_schedblocksize == 0);
+	
+    // How many times to generate a DSP event in PD
+    int times = inNumberFrames / sys_schedblocksize;
+	
+    for(int i = 0; i < times; i++) {
+        
+		
+		for (int n = 0; n < sys_schedblocksize; n++) {
+            for(int ch = 0; ch < sys_ninchannels; ch++) {
+				sys_soundin[n+sys_schedblocksize*ch] =   input[(n*sys_ninchannels+ch) + // offset in iteration
+															   i*sys_schedblocksize*sys_ninchannels]; // iteration starting address
+				
+				
+            }        
+        }
+		
+		
+		// do one Pd DSP block
+        sys_lock();
+		sched_audio_callbackfn();
+        sys_unlock();
+		
+        // This should cover sys_noutchannels channels. Turns non-interleaved into 
+        // interleaved audio.
+        for (int n = 0; n < sys_schedblocksize; n++) {
+            for(int ch = 0; ch < sys_noutchannels; ch++) {
+                t_sample fsample = CLAMP(sys_soundout[n+sys_schedblocksize*ch],-1,1);
+                output[(n*sys_noutchannels+ch) + // offset in iteration
+					   i*sys_schedblocksize*sys_noutchannels] // iteration starting address
+				
+				
+				
+				= fsample;
+				//sys_soundin[n+sys_schedblocksize*ch] = ofRandom(-0.5,0.5); // iteration starting address
+				
+			}        
+        }
+        
+        // After loading the samples, we need to clear them for the next iteration
+        memset(sys_soundout, 0, sizeof(t_sample)*sys_noutchannels*sys_schedblocksize);        
+    }
+}
--- src/ofxPd.h
+++ src/ofxPd.h
@@ -1,11 +1,44 @@
 /*
- *  ofxPd.h
- *  luciolinae_ctl
- *
- *  Created by damian on 12/08/10.
- *  Copyright 2010 frey damian@frey.co.nz. All rights reserved.
- *
+ 
+ ofxPd v0.02 
+ Copyright 2010 by Damian Stewart, Zach Liebermann.
+ 
+ this code uses code from pdlib "AudioOutput.h", so pdlib license is included here: 
+ 
+ AudioOutput.h
+ PdLib v0.3
+ 
+ Copyright 2010 Bryan Summersett. All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are
+ permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this list of
+ conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list
+ of conditions and the following disclaimer in the documentation and/or other materials
+ provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY BRYAN SUMMERSETT ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL BRYAN SUMMERSETT OR
+ CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ The views and conclusions contained in the software and documentation are those of the
+ authors and should not be interpreted as representing official policies, either expressed
+ or implied, of Bryan Summersett.
+ 
+ 
+ 
  */
+
+
 
 #include "ofMain.h"
 #include "ofxThread.h"
@@ -15,8 +48,10 @@ class ofxPd : public ofxThread
 public:	
 	
 	/// lib_dir is the directory in which to look for pd files
-	/// out_chans, bitrate and block_size are passed to Pd engine
-	void setup( string lib_dir, int out_chans = 2, int bitrate = 44100, int block_size = 64 );
+	/// in_chans, out_chans, bitrate and block_size are passed to Pd engine
+	/// if in_chans is >0, you should call the input+output version of renderAudio to pass
+	/// input audio to the Pd engine
+	void setup( string lib_dir, int in_chans = 0, int out_chans = 2, int bitrate = 44100, int block_size = 64 );
 	
 	/// add the given file to the list to be opened on startup
 	void addOpenFile( string file_path );
@@ -29,8 +64,10 @@ public:
 	/// stop pd
 	void stop();
 	
-	/// callback for audio rendering
+	/// callback for audio rendering (output only)
 	void renderAudio( float* output, int bufferSize, int nChannels );
+	/// callback for audio rendering (input and output)
+	void renderAudio( float * input, float* output, int bufferSize, int nChannels );
 	
 private:
 
@@ -44,6 +81,7 @@ private:
 	vector <string> open_files;
 	
 	int out_chans;
+	int in_chans;
 	int bitrate;
 	int block_size;
 	
